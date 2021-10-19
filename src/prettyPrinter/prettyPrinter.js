8af9d5ac3f2b783195299f05870b9067551dd133
Oliwer <oe222ez@student.lnu.se>
2021-10-19 16:28:56 +0200

    Add color and index for each sentence in PrettyPrinter

--- src/prettyPrinter/prettyPrinter.js
+++ src/prettyPrinter/prettyPrinter.js
@@ -4,16 +4,46 @@ export class PrettyPrinter {
   #document = new Document()
 
   start(input) {
-    this.#document.parse(input)
-    this.#printDocument()
+    try {
+      this.#document.parse(input)
+      this.#setupPrintDocument()
+    } catch (err) {
+      console.error(err.message)
+    }
   }
 
-  #printDocument() {
+  #setupPrintDocument() {
     const sentences = this.#getSentences()
-    console.log(sentences)
+    this.#printDocument(sentences)
   }
 
   #getSentences() {
     return this.#document.getAllSentences()
+  }
+
+  #printDocument(sentences) {
+    for (let i = 0; i < sentences.length; i++) {
+      if (sentences[i].getEndType() === 'DOT') {
+        this.#printDotSentence(i, sentences[i])
+      } else if (sentences[i].getEndType() === 'QUESTION') {
+        this.#printQuestionSentence(i, sentences[i])
+      } else if (sentences[i].getEndType() === 'EXCLAMATION') {
+        this.#printExclamationSentence(i, sentences[i])
+      } else {
+        throw new Error(`${sentences[i].getEndType()} is not a valid end type.`)
+      }
+    }
+  }
+
+  #printDotSentence(index, sentence) {
+    console.log(`${index}.` + '\x1b[32m', sentence.getSentenceString() + '\x1b[0m') // green
+  }
+
+  #printQuestionSentence(index, sentence) {
+    console.log(`${index}.` + '\x1b[31m', sentence.getSentenceString() + '\x1b[0m') // red
+  }
+
+  #printExclamationSentence(index, sentence) {
+    console.log(`${index}.` + '\x1b[4m', sentence.getSentenceString() + '\x1b[0m') // underscore
   }
 }
