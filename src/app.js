c5cafe5459bc91da234245e9e0bcc59e7a7a26d5
Oliwer <oe222ez@student.lnu.se>
2021-10-19 16:37:27 +0200

    Remove comments

--- src/app.js
+++ src/app.js
@@ -4,83 +4,14 @@ import { PrettyPrinter } from "./prettyPrinter/prettyPrinter.js"
 
 const prettyPrinter = new PrettyPrinter()
 
-const input = 'Meningen består. Gör den? JA!'
+const input = 'Detta är en mening. Är det? JA!'
 
 prettyPrinter.start(input)
 
 
-
-
-
-
-
-/*
-const document = new Document()
-
-const string = 'Meningen består av ord. Det gör även denna meningen.'
-const string2 = 'Meningen består. Gör den? JA!'
-const string3 = 'Meningen består.'
-document.parse(string2)
-
-const sentencesObjects = document.getAllSentences()
-
-console.log(sentencesObjects)
-
-console.log('----')
-
-console.log(sentencesObjects[0].getSentenceString())
-console.log(sentencesObjects[1].getSentenceString())
-console.log(sentencesObjects[2].getSentenceString())
-
-console.log('----')
-
-console.log(document.getAllDotSentences())
-console.log(document.getAllQuestionSentences())
-console.log(document.getAllExclamationSentences())
-
-console.log('----')
-
-console.log(document.getAllDotSentences()[0].getSentenceString())
-console.log(document.getAllQuestionSentences()[0].getSentenceString())
-console.log(document.getAllExclamationSentences()[0].getSentenceString())
-*/
-/*
-console.log(sentencesObjects[0].getSentenceString())
-console.log(sentencesObjects[0].getEndType())
-
-console.log(sentencesObjects[0].getActiveSentenceToken())
-
-sentencesObjects[0].setNextActiveSentenceToken()
-console.log(sentencesObjects[0].getActiveSentenceToken())
-
-sentencesObjects[0].setNextActiveSentenceToken()
-console.log(sentencesObjects[0].getActiveSentenceToken())
-
-sentencesObjects[0].setNextActiveSentenceToken()
-console.log(sentencesObjects[0].getActiveSentenceToken())
-
-sentencesObjects[0].setPrevActiveSentenceToken()
-console.log(sentencesObjects[0].getActiveSentenceToken())
-
-sentencesObjects[0].setPrevActiveSentenceToken()
-console.log(sentencesObjects[0].getActiveSentenceToken())
-
-sentencesObjects[0].setPrevActiveSentenceToken()
-console.log(sentencesObjects[0].getActiveSentenceToken())
-
-sentencesObjects[0].setPrevActiveSentenceToken()
-console.log(sentencesObjects[0].getActiveSentenceToken())
-*/
-
-
-// console.log(sentencesObjects[1].getSentenceString())
-// console.log(sentencesObjects[1].getEndType())
-
-// console.log(sentencesObjects[2].getSentenceString())
-// console.log(sentencesObjects[2].getEndType())
-
 /*
 
  * Bugg: två meningar med samma slut token (.?!) efter varabdra blir oändlig loop.
+ * Även om de inte är efter varandra!
 
 */
