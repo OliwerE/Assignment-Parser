b30daf9d63208753296fd5dedaa3fd8a9cb8a37e
Oliwer <oe222ez@student.lnu.se>
2021-10-21 19:25:05 +0200

    Change test results to table

--- tests/startTests.js
+++ tests/startTests.js
@@ -2,40 +2,127 @@ import * as sentenceTests from './sentenceTests.js'
 import * as sentencesTests from './sentencesTests.js'
 import * as documentTests from './documentTests.js'
 
-const handlePass = (isPass) => {
-  return (isPass ? '\x1b[32m' + 'PASS' + '\x1b[0m' : '\x1b[31m' + 'FAIL' + '\x1b[0m')
-}
-
-const runTests = () => {
+const runParserAndPrinterTests = () => {
 
   console.log('----- Sentence Tests -----')
-  console.log('tc1: Returnera mening, Indata: "En mening.", Förväntat: "En mening.": ' + handlePass(sentenceTests.tc1()))
-  console.log('tc2: Returnera en fråga, Indata: "En mening?", Förväntat: "En mening?": ' + handlePass(sentenceTests.tc2()))
-  console.log('tc3: Returnera ett utrop, Indata: "En mening!", Förväntat: "En mening!": ' + handlePass(sentenceTests.tc3()))
-  console.log('tc4: Få end token typ DOT, Indata: "En mening.", Förväntat: "DOT": ' + handlePass(sentenceTests.tc4()))
-  console.log('tc5: Få end token typ DOT, Indata: "a? b! c.", Förväntat: "DOT": ' + handlePass(sentenceTests.tc5()))
-  console.log('tc6: Få end token typ QUESTION, Indata: "En mening?", Förväntat: "QUESTION": ' + handlePass(sentenceTests.tc6()))
-  console.log('tc7: Få end token typ QUESTION, Indata: "a. b! c?", Förväntat: "QUESTION": ' + handlePass(sentenceTests.tc7()))
-  console.log('tc8: Få end token typ EXCLAMATION, Indata: "En mening!", Förväntat: "EXCLAMATION": ' + handlePass(sentenceTests.tc8()))
-  console.log('tc9: Få end token typ EXCLAMATION, Indata: "a. b? c!", Förväntat: "EXCLAMATION": ' + handlePass(sentenceTests.tc9()))
-  console.log('tc10: Stega till nästa token, Indata: "En mening.", Input: ">", Förväntat: värde: "mening", typ: "WORD": ' + handlePass(sentenceTests.tc10()))
-  console.log('tc11: Stega fram sedan tillbaks token, Indata: "En mening.", Input: "><", Förväntat: värde: "En", typ: "WORD": ' + handlePass(sentenceTests.tc11()))
-  console.log('tc12: Stega bakåt till sista token, Indata: "En mening.", Input: "<", Förväntat: värde: ".", typ: "DOT": ' + handlePass(sentenceTests.tc12()))
-  console.log('tc13: Stega fram tills första token är aktiv igen, Indata: "En mening.", Input: ">>>", Förväntat: värde: "En", typ: "WORD": ' + handlePass(sentenceTests.tc13()))
-  console.log('tc14: Stega bakåt tills första token är aktiv igen, Indata: "En mening.", Input: "<<<", Förväntat: värde: "En", typ: "WORD": ' + handlePass(sentenceTests.tc14()))
-  
+  console.table({ 'tc1: Returnera mening': {
+    Indata: '"En mening."',
+    Input: '-',
+    Förväntat: '"En mening."',
+    Resultat: (sentenceTests.tc1() ? 'PASS' : 'FAIL')
+  },
+  'tc2: Returnera en fråga': {
+    Indata: '"En mening?"',
+    Input: '-',
+    Förväntat: '"En mening?"',
+    Resultat: (sentenceTests.tc2() ? 'PASS' : 'FAIL')
+  },
+  'tc3: Returnera ett utrop': {
+    Indata: '"En mening!"',
+    Input: '-',
+    Förväntat: '"En mening!"',
+    Resultat: (sentenceTests.tc3() ? 'PASS' : 'FAIL')
+  },
+  'tc4: Få end token typ DOT': {
+    Indata: '"En mening."',
+    Input: '-',
+    Förväntat: '"DOT"',
+    Resultat: (sentenceTests.tc4() ? 'PASS' : 'FAIL')
+  },
+  'tc5: Få end token typ DOT': {
+    Indata: '"a? b! c."',
+    Input: '-',
+    Förväntat: '"DOT"',
+    Resultat: (sentenceTests.tc5() ? 'PASS' : 'FAIL')
+  },
+  'tc6: Få end token typ QUESTION': {
+    Indata: '"En mening?',
+    Input: '-',
+    Förväntat: '"QUESTION"',
+    Resultat: (sentenceTests.tc6() ? 'PASS' : 'FAIL')
+  },
+  'tc7: Få end token typ QUESTION': {
+    Indata: '"a. b! c?"',
+    Input: '-',
+    Förväntat: '"QUESTION"',
+    Resultat: (sentenceTests.tc6() ? 'PASS' : 'FAIL')
+  },
+  'tc8: Få end token typ EXCLAMATION': {
+    Indata: '"En mening!"',
+    Input: '-',
+    Förväntat: '"EXCLAMATION"',
+    Resultat: (sentenceTests.tc8() ? 'PASS' : 'FAIL')
+  },
+  'tc9: Få end token typ EXCLAMATION': {
+    Indata: '"a. b? c!"',
+    Input: '-',
+    Förväntat: '"EXCLAMATION"',
+    Resultat: (sentenceTests.tc9() ? 'PASS' : 'FAIL')
+  },
+  'tc10: Stega till nästa token': {
+    Indata: '"En mening."',
+    Input: '>',
+    Förväntat: 'värde: "mening", typ: "WORD"',
+    Resultat: (sentenceTests.tc10() ? 'PASS' : 'FAIL')
+  },
+  'tc11: Stega fram sedan tillbaks token': {
+    Indata: '"En mening."',
+    Input: '><',
+    Förväntat: 'värde: "En", typ: "WORD"',
+    Resultat: (sentenceTests.tc11() ? 'PASS' : 'FAIL')
+  },
+  'tc12: Stega bakåt till sista token': {
+    Indata: '"En mening."',
+    Input: '<',
+    Förväntat: 'värde: ".", typ: "DOT"',
+    Resultat: (sentenceTests.tc12() ? 'PASS' : 'FAIL')
+  },
+  'tc13: Stega fram tills första token är aktiv igen': {
+    Indata: '"En mening."',
+    Input: '>>>',
+    Förväntat: 'värde: "En", typ: "WORD"',
+    Resultat: (sentenceTests.tc13() ? 'PASS' : 'FAIL')
+  },
+  'tc14: Stega bakåt tills första token är aktiv igen': {
+    Indata: '"En mening."',
+    Input: '<<<',
+    Förväntat: 'förv',
+    Resultat: (sentenceTests.tc14() ? 'PASS' : 'FAIL')
+  }
+  })
+
   console.log('\n----- Sentences Tests -----')
-  console.log('tc1: Returnera alla meningar, Indata: "a. b? c!", Förväntat: "Array med tre meningar: a., b? och c!": ' + handlePass(sentencesTests.tc1()))
+  console.table({ 'tc1: Returnera alla meningar': {
+      Indata: '"a. b? c!"',
+      Förväntat: 'Array med tre meningar: a., b? och c!',
+      Resultat: (sentencesTests.tc1() ? 'PASS' : 'FAIL')
+    }
+  })
 
   console.log('\n----- Document Tests -----')
-  console.log('tc1: Returnera alla meningar, Indata: "a! b? c. d? e! f.", Förväntat: "Array som innehåller meningarna: a!, b?, c., d?, e!, f.": ' + handlePass(documentTests.tc1()))
-  console.log('tc2: Returnera alla meningar med punkt, Indata: "a! b? c. d? e! f.", Förväntat: "Array som innehåller meningarna: c., f.": ' + handlePass(documentTests.tc2()))
-  console.log('tc3: Returnera alla frågor, Indata: "a! b? c. d? e! f.", Förväntat: "Array som innehåller meningarna: b?, d?": ' + handlePass(documentTests.tc3()))
-  console.log('tc4: Returnera alla meningar med utropstecken, Indata: "a! b? c. d? e! f.", Förväntat: "Array som innehåller meningarna: a!, e!": ' + handlePass(documentTests.tc4()))
+  console.table({ 'tc1: Returnera alla meningar': {
+      Indata: '"a! b? c. d? e! f."',
+      Förväntat: 'Array som innehåller meningarna: a!, b?, c., d?, e!, f.',
+      Resultat: (documentTests.tc1() ? 'PASS' : 'FAIL')
+    },
+    'tc2: Returnera alla meningar med punkt': {
+      Indata: '"a! b? c. d? e! f."',
+      Förväntat: 'Array som innehåller meningarna: c., f.',
+      Resultat: (documentTests.tc2() ? 'PASS' : 'FAIL')
+    },
+    'tc3: Returnera alla frågor': {
+      Indata: '"a! b? c. d? e! f."',
+      Förväntat: 'Array som innehåller meningarna: b?, d?',
+      Resultat: (documentTests.tc3() ? 'PASS' : 'FAIL')
+    },
+    'tc4: Returnera alla meningar med utropstecken': {
+      Indata: '"a! b? c. d? e! f."',
+      Förväntat: 'Array som innehåller meningarna: a!, e!',
+      Resultat: (documentTests.tc4() ? 'PASS' : 'FAIL')
+    }
+  })
 
   console.log('\n----- Pretty Printer Tests -----')
   console.log('Manuella tester finns i README.md')
-
-
 }
-runTests()
+runParserAndPrinterTests()
